24880b1e57366dcd95f1a3a83e50028f999f3457
Murilo Heyde <murilo.heyde89@gmail.com>
2022-11-12 12:57:08 -0300

    atualizar

--- web_project/urls.py
+++ web_project/urls.py
@@ -7,6 +7,8 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 from . import views
 
+
+
 urlpatterns = [
 	path('proposta/<int:id>/', views.proposta, name='proposta'),
     path('admin/', admin.site.urls),
