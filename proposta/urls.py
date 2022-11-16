f09fc52ba2852b15ff70124fb41a29ecfa47887f
Murilo Heyde <murilo.heyde89@gmail.com>
2022-11-16 11:39:23 -0300

    atualizado ok

--- proposta/urls.py
+++ proposta/urls.py
@@ -15,4 +15,4 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns(urlpatterns)
