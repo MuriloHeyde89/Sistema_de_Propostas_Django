b9d5415a1c55dc13b06a3323ea779aeb33d734bc
Murilo Heyde <murilo.heyde89@gmail.com>
2022-11-16 15:07:53 -0300

    Atualizado

--- proposta/urls.py
+++ proposta/urls.py
@@ -6,9 +6,12 @@ from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 from django.conf import settings
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-	path('proposta/<int:id>/', views.proposta, name='proposta'),
+	    path('proposta/<int:id>/', views.proposta, name='proposta'),
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
